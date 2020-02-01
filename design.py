6048ee955ed8d79638f0e907101627a2964d539f
randomkid500 <60551332+randomkid500@users.noreply.github.com>
2020-02-01 10:46:28 -0800

    Update design.py

--- design.py
+++ design.py
@@ -1,8 +1,8 @@
 import pygame
-background_colour = (255,255,255)
-(width, height) = (300, 200)
+background_colour = (32, 118, 176)
+(width, height) = (500, 500)
 screen = pygame.display.set_mode((width, height))
-pygame.display.set_caption('Tutorial 1')
+pygame.display.set_caption('Design')
 screen.fill(background_colour)
 pygame.display.flip()
 running = True

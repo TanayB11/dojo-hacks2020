1de5ea7ff06b9218ed5c44663a6447695787a7f4
yashrajana04 <yashrajana04@gmail.com>
2020-02-01 11:04:54 -0800

    Update design.py

--- design.py
+++ design.py
@@ -1,6 +1,11 @@
 import pygame
+<<<<<<< HEAD
+background_colour = (255,255,255)
+(width, height) = (300, 200)
+=======
 background_colour = (255, 255, 255)
 (width, height) = (500, 500)
+>>>>>>> b7d4b498350ec6a4eca12a5d4009484d4f98b413
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption('Design')
 screen.fill(background_colour)
@@ -10,3 +15,16 @@ while running:
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       running = False
+class Drawing(RelativeLayout):
+
+    # On mouse press how Paint_brush behave
+    def on_touch_down(self, touch):
+        pb = Paint_brush()
+        pb.center = touch.pos
+        self.add_widget(pb)
+
+    # On mouse movement how Paint_brush behave
+    def on_touch_move(self, touch): 
+        pb = Paint_brush()
+        pb.center = touch.pos
+        self.add_widget(pb)

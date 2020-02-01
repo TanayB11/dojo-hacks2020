606e46d6b9fc426da3bff977510797e04450b397
TanayB11 <tanaybiradar24@gmail.com>
2020-02-01 11:03:56 -0800

    Add preprocessing code

--- src/train.py
+++ src/train.py
@@ -5,6 +5,7 @@ from tensorflow.python.keras.layers.core import Dense, Dropout, Flatten
 from tensorflow.python.keras.models import Sequential
 from tensorflow.python.keras.callbacks import ModelCheckpoint, TensorBoard
 
+# Define the model
 model = Sequential()
 model.add(Conv2D(64, (3, 3),
     activation='relu', padding='same', strides=(1, 1),
@@ -18,21 +19,21 @@ model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
 model.add(Flatten())
 model.add(Dense(256, activation='relu'))
 model.add(Dropout(0.1))
-model.add(Dense(100, activation='softmax')) # TODO - Classes
+model.add(Dense(100, activation='softmax'))
 model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 model.summary()
 
 with open ('../model/model.json', 'w') as f:
     f.write(model.to_json())
 
-# Load preprocessed training data
-with h5py.File('../data/temp100/compressed/trainX.h5', 'r') as f:
+# Load training data
+with h5py.File('../data/compressed/trainX.h5', 'r') as f:
     trainX = f['trainX'][:]
-with h5py.File('../data/temp100/compressed/testX.h5', 'r') as f:
+with h5py.File('../data/compressed/testX.h5', 'r') as f:
     testX = f['testX'][:]
-with h5py.File('../data/temp100/compressed/trainY.h5', 'r') as f:
+with h5py.File('../data/compressed/trainY.h5', 'r') as f:
     trainY = f['trainY'][:]
-with h5py.File('../data/temp100/compressed/testY.h5', 'r') as f:
+with h5py.File('../data/compressed/testY.h5', 'r') as f:
     testY = f['testY'][:]
 print(trainX.shape, testX.shape, trainY.shape, testY.shape)
 
@@ -42,7 +43,7 @@ checkpoint = ModelCheckpoint(weights_path, monitor='val_accuracy', verbose=1, sa
 tensorboard_callback = TensorBoard(log_dir=log_dir, histogram_freq=1)
 callbacks_list = [checkpoint, tensorboard_callback]
 
-model.fit(trainX, trainY, epochs=10, batch_size=128, validation_data=(testX, testY), verbose=1, callbacks=callbacks_list, shuffle=True)
+model.fit(trainX, trainY, epochs=10, batch_size=64, validation_data=(testX, testY), verbose=1, callbacks=callbacks_list, shuffle=True)
 model.save_weights(weights_path)
 score = model.evaluate(testX, testY, verbose=0)
 print('Test score:', score[0], '\n', 'Test accuracy:', score[1])
